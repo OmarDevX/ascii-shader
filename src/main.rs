007c7141dbb23c53f001946d08cce62c3e30dedc
OmarDevX <omardevxme@gmail.com>
2024-07-17 10:35:54 +0300

    made seprate file for the shader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::ffi::CString;
+use std::fs;
 use std::ptr;
 use std::str;
 use gl::types::*;
@@ -29,37 +30,8 @@ async fn create_and_modify_image(grid: &mut Vec<(String, (u8, u8, u8))>) -> Vec<
     window.make_current();
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    // OpenGL Compute Shader setup
-    let compute_shader_src = r#"
-    #version 430
-
-    layout (local_size_x = 1, local_size_y = 1) in;
-
-    layout (binding = 0, rgba8) uniform image2D img_output;
-
-    void main() {
-        ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
-        vec2 center = vec2(imageSize(img_output)) / 2.0;
-        float radius = float(min(imageSize(img_output).x, imageSize(img_output).y)) / 2.0;
-
-        vec3 light_dir = normalize(vec3(1.0, 1.0, 1.0));
-        vec3 sphere_color = vec3(255.0 / 255.0, 127.0 / 255.0, 51.0 / 255.0);
-
-        vec2 dist = vec2(pixel_coords) - center;
-        float dist_length = length(dist);
-
-        vec4 color = vec4(0.0);
-        if (dist_length <= radius) {
-            float z = sqrt(radius * radius - dist_length * dist_length);
-            vec3 normal = normalize(vec3(dist, z));
-            float diffuse = max(dot(normal, light_dir), 0.0);
-            vec3 shaded_color = sphere_color * diffuse;
-            color = vec4(shaded_color, 1.0);
-        }
-
-        imageStore(img_output, pixel_coords, color);
-    }
-    "#;
+    let compute_shader_src = fs::read_to_string("shader/compute_shader.glsl")
+        .expect("Failed to read compute shader file");
 
     unsafe {
         // Create and compile compute shader
